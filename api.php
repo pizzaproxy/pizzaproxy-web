26836729f4c61b22fdc3ba6a115c365d37980a6d
Sebastian Muszytowski <code@muszytowski.net>
2011-07-24 19:37:19 +0200

    add some new functions, see pizzaproxy-orga/ToDo

---
+++ api.php
@@ -0,0 +1,17 @@
+<?php
+
+/* Config */
+$config["alternatives_visible"] = 3;
+/* Config */
+
+include dirname(__FILE__) . '/lib/database.php';
+include dirname(__FILE__) . '/lib/Order.php';
+include dirname(__FILE__) . '/lib/OrderProxy.php';
+include dirname(__FILE__) . '/lib/Pizza.php';
+include dirname(__FILE__) . '/lib/ProxyPizza.php';
+include dirname(__FILE__) . '/lib/PizzaService.php';
+include dirname(__FILE__) . '/lib/helper.class.php';
+
+?>
+
+
---
+++ setup.php
@@ -0,0 +1,28 @@
+<?php
+
+include dirname(__FILE__) . '/lib/database.php';
+include dirname(__FILE__) . '/lib/Order.php';
+include dirname(__FILE__) . '/lib/Pizza.php';
+include dirname(__FILE__) . '/lib/ProxyPizza.php';
+include dirname(__FILE__) . '/lib/PizzaService.php';
+include dirname(__FILE__) . '/lib/OrderProxy.php';
+
+$setupok = Database::createTables();
+
+?>
+
+<html>
+<head>
+<title>
+PizzaProxy
+</title>
+<meta http-equiv="REFRESH" content="10;url=index.php">
+</head>
+<body>
+<?php if($setupok):?>
+  Setup erfolgreich! :)
+<?php else:?>
+  Oops! Setup fehlgeschlagen :(
+<?php endif;?>
+</body>
+</html>
