d61d8079e85f12a4b0dd9ed11c249893bffd2d88
Sebastian Muszytowski <code@muszytowski.net>
2011-07-27 15:06:39 +0200

    Heureka, better display

--- templates/order/printSuccess.php
+++ templates/order/printSuccess.php
@@ -54,7 +54,7 @@ PizzaProxy
         <td style="vertical-align: top;"><?php echo helper::formatPrice($order["numpizza"]*$order["price"])?></td>
         <td class="check">
                 <?php for ($a = 1; $a <= $order["numpizza"]; $a++):?>
-                    <div><input type="checkbox" style="display: inline; height: 20px; width: 20px;"></div>
+                    <div>&nbsp;&nbsp;&nbsp;</div>
                 <?php endfor;?>
         </td>
       </tr>
