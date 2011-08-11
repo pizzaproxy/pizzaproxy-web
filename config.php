2f751d51e7e5e54cb104ead41e823559be75c5f9
Sebastian Muszytowski <code@muszytowski.net>
2011-08-11 19:59:13 +0200

    renamed Database, add config, modified css

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    "event" => "orr2011",
+    "logofile" => "orr.png",
+    "apiuser" => 
+        array(        
+                "cui" => "f1d2d2f924e986ac86fdf7b36c94bcdf32beec15",
+                "reg" => "e242ed3bffccdf271b7fbaf34ed72d089537b42f"
+        ),
+    "alternatives_visible" => 3
+);
+
+?>
