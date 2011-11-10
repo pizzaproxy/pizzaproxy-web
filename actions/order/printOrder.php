e2791e66c43c578f89e8e1db22805a30e07eb8de
Sebastian Muszytowski <smuszytowski@smuszytowski.(none)>
2011-11-10 09:31:04 +0100

    no idea if it will work

--- actions/order/printOrder.php
+++ actions/order/printOrder.php
@@ -105,7 +105,7 @@ if($ok == 1){
 	$html = '<html><body><script type="text/javascript">window.opener.location.reload();window.close();</script></body></html>';
 	//$html = var_dump(Order::getOrder(11));
 	$latexorderids = array();
-	foreach ($orders as $order){
+	foreach ($limitedOrders as $order){
 	  $latexorderids[] = $order["orderid"];
 	}
 	Order::markAsOrdered($latexorderids,$orderSerivce["id"]);
